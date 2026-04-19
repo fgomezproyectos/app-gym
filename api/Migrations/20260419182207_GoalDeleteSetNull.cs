4a77752fe45b450afea8ee5287665854450f077d
fran <frangomez.proyecto@gmail.com>
2026-04-19 20:33:56 +0200

    Funcionalidad Eliminar Goals

---
+++ api/Migrations/20260419182207_GoalDeleteSetNull.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GymApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class GoalDeleteSetNull : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DailyGoalLogs_Goals_GoalId",
+                table: "DailyGoalLogs");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DailyGoalLogs_Goals_GoalId",
+                table: "DailyGoalLogs",
+                column: "GoalId",
+                principalTable: "Goals",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DailyGoalLogs_Goals_GoalId",
+                table: "DailyGoalLogs");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DailyGoalLogs_Goals_GoalId",
+                table: "DailyGoalLogs",
+                column: "GoalId",
+                principalTable: "Goals",
+                principalColumn: "Id");
+        }
+    }
+}
